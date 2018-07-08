e768389a5b4b31592745d4fe5485944f4c90f170
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-07-08 21:25:19 +0900

    scanf(3)を避けてfgetc(3)を使用して整数値を読み込むようにした。

--- main.c
+++ main.c
@@ -1,13 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+enum {
+    tINT,
+};
+
+typedef struct {
+    int kind;
+
+    union {
+        int ival;
+    };
+} Token;
+
+Token read_next_token()
+{
+    char buf[256];  // TODO: enough length?
+    int bufidx = 0;
+
+    while (1) {
+        int ch;
+
+        ch = fgetc(stdin);
+        if (ch == EOF) {
+            Token token;
+
+            buf[bufidx++] = '\0';
+            token.kind = tINT;
+            token.ival = atoi(buf);
+            return token;
+        }
+
+        buf[bufidx++] = ch;
+    }
+}
 
 int main()
 {
-    int input;
+    Token token;
 
-    scanf("%d", &input);
+    token = read_next_token();
+
     puts(".global main");
     puts("main:");
-    printf("\tmov $%d, %%eax\n", input);
+    printf("\tmov $%d, %%eax\n", token.ival);
     puts("\tret");
 
     return 0;
