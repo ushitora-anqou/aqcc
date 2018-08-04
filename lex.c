bed41d5fd52fa866657ac4a653d0e38e03dd9917
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-08-04 15:32:12 +0900

    字句解析を、ファイルを全部読み込んでから行うようにした。

--- lex.c
+++ lex.c
@@ -1,5 +1,16 @@
 #include "aqcc.h"
 
+typedef struct {
+    int line, column;
+    char *src;
+} SrcFile;
+
+char getch(SrcFile *file) { return *file->src++; }
+
+void ungetch(SrcFile *file) { file->src--; }
+
+char peekch(SrcFile *file) { return *file->src; }
+
 Token *new_token(int kind)
 {
     Token *token = safe_malloc(sizeof(Token));
@@ -7,46 +18,32 @@ Token *new_token(int kind)
     return token;
 }
 
-char fpeekc(FILE *fh)
+Token *read_next_int_token(SrcFile *file)
 {
-    char ch = fgetc(fh);
-    ungetc(ch, fh);
-    return ch;
-}
+    int acc = 0;
 
-Token *read_next_int_token(FILE *fh)
-{
-    int acc;
-
-    acc = 0;
-
-    while (isdigit(fpeekc(fh))) acc = 10 * acc + fgetc(fh) - '0';
+    while (isdigit(peekch(file))) acc = 10 * acc + getch(file) - '0';
 
     Token *token = new_token(tINT);
     token->ival = acc;
     return token;
 }
 
-Token *read_next_ident_token(FILE *fh)
+Token *read_next_ident_token(SrcFile *file)
 {
-    StringBuilder *sb;
-    char *str;
-
-    sb = new_string_builder();
-
-    string_builder_append(sb, fgetc(fh));
-
+    StringBuilder *sb = new_string_builder();
     while (1) {
-        int ch = fgetc(fh);
+        int ch = getch(file);
 
         if (!isalnum(ch) && ch != '_') {
-            ungetc(ch, fh);
+            ungetch(file);
             break;
         }
 
         string_builder_append(sb, ch);
     }
 
+    char *str;
     str = string_builder_get(sb);
 
     if (strcmp(str, "return") == 0) return new_token(kRETURN);
@@ -66,165 +63,185 @@ Token *read_next_ident_token(FILE *fh)
 }
 
 // assume that the first doublequote has been already read.
-Token *read_next_string_literal_token(FILE *fh)
+Token *read_next_string_literal_token(SrcFile *file)
 {
-    StringBuilder *sb;
-    Token *token;
-
-    sb = new_string_builder();
-
+    StringBuilder *sb = new_string_builder();
     while (1) {
-        int ch;
-
-        ch = fgetc(fh);
+        char ch = getch(file);
         if (ch == '"') break;
-        if (ch == EOF) error("unexpected EOF", __FILE__, __LINE__);
+        if (ch == '\0') error("unexpected EOF", __FILE__, __LINE__);
         if (ch == '\n')
             error("unexpected new-line character", __FILE__, __LINE__);
 
         string_builder_append(sb, ch);
     }
 
+    Token *token;
     token = new_token(tSTRING_LITERAL);
     token->sval = string_builder_get(sb);
     return token;
 }
 
-Token *read_next_token(FILE *fh)
+Token *read_next_token(SrcFile *file)
 {
-    while (1) {
-        int ch;
-
-        ch = fgetc(fh);
+    while (peekch(file) != '\0') {
+        char ch = getch(file);
 
         if (isspace(ch)) continue;
 
         if (isdigit(ch)) {
-            ungetc(ch, fh);
-            return read_next_int_token(fh);
+            ungetch(file);
+            return read_next_int_token(file);
         }
 
         if (isalpha(ch) || ch == '_') {
-            ungetc(ch, fh);
-            return read_next_ident_token(fh);
+            ungetch(file);
+            return read_next_ident_token(file);
         }
 
         switch (ch) {
             case '"':
-                return read_next_string_literal_token(fh);
+                return read_next_string_literal_token(file);
 
             case '+':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '+') return new_token(tINC);
                 if (ch == '=') return new_token(tPLUSEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tPLUS);
+
             case '-':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tMINUSEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tMINUS);
+
             case '*':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tSTAREQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tSTAR);
+
             case '/':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tSLASHEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tSLASH);
+
             case '%':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tPERCENTEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tPERCENT);
+
             case '(':
                 return new_token(tLPAREN);
+
             case ')':
                 return new_token(tRPAREN);
+
             case '<':
-                ch = fgetc(fh);
+                ch = getch(file);
                 switch (ch) {
                     case '<':
-                        ch = fgetc(fh);
+                        ch = getch(file);
                         if (ch == '=') return new_token(tLSHIFTEQ);
-                        ungetc(ch, fh);
+                        ungetch(file);
                         return new_token(tLSHIFT);
                     case '=':
                         return new_token(tLTE);
                 }
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tLT);
+
             case '>':
-                ch = fgetc(fh);
+                ch = getch(file);
                 switch (ch) {
                     case '>':
-                        ch = fgetc(fh);
+                        ch = getch(file);
                         if (ch == '=') return new_token(tRSHIFTEQ);
-                        ungetc(ch, fh);
+                        ungetch(file);
                         return new_token(tRSHIFT);
                     case '=':
                         return new_token(tGTE);
                 }
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tGT);
+
             case '=':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tEQEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tEQ);
+
             case '!':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch != '=') break;
                 return new_token(tNEQ);
+
             case '&':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '&') return new_token(tANDAND);
                 if (ch == '=') return new_token(tANDEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tAND);
+
             case '^':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '=') return new_token(tHATEQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tHAT);
+
             case '|':
-                ch = fgetc(fh);
+                ch = getch(file);
                 if (ch == '|') return new_token(tBARBAR);
                 if (ch == '=') return new_token(tBAREQ);
-                ungetc(ch, fh);
+                ungetch(file);
                 return new_token(tBAR);
+
             case ';':
                 return new_token(tSEMICOLON);
+
             case ',':
                 return new_token(tCOMMA);
+
             case '{':
                 return new_token(tLBRACE);
+
             case '}':
                 return new_token(tRBRACE);
+
             case ':':
                 return new_token(tCOLON);
+
             case '?':
                 return new_token(tQUESTION);
+
             case '[':
                 return new_token(tLBRACKET);
+
             case ']':
                 return new_token(tRBRACKET);
-            case EOF:
-                return new_token(tEOF);
         }
 
         error("unexpected token", __FILE__, __LINE__);
     }
+
+    return new_token(tEOF);
 }
 
 Vector *read_all_tokens(FILE *fh)
 {
-    Vector *tokens = new_vector();
+    // read the file all
+    StringBuilder *sb = new_string_builder();
+    int ch;
+    while ((ch = fgetc(fh)) != EOF) string_builder_append(sb, ch);
+    SrcFile file;
+    file.src = string_builder_get(sb);
 
+    Vector *tokens = new_vector();
     while (1) {
-        Token *token = read_next_token(fh);
+        Token *token = read_next_token(&file);
         vector_push_back(tokens, token);
         if (token->kind == tEOF) break;
     }
