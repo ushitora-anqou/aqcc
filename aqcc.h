550da145823a9bb431256a856cf884aacdb51830
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-08-12 11:54:29 +0900

    va_listの定義に含まれているunsignedを削除。

--- aqcc.h
+++ aqcc.h
@@ -13,8 +13,8 @@
 #define va_arg __builtin_va_arg
 
 typedef struct {
-    unsigned int gp_offset;
-    unsigned int fp_offset;
+    int gp_offset;
+    int fp_offset;
     void *overflow_arg_area;
     void *reg_save_area;
 } va_list[1];
