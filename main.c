cfba75e0b005d7879de3cbe878afe9eb830969f6
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-09-05 11:50:49 +0900

    detect in/out file type by file extension

--- main.c
+++ main.c
@@ -9,44 +9,58 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    if (argc < 3) error("Usage: aqcc [-S] input-file-path output-file-path");
+    if (argc < 3) error("Usage: aqcc input-file-path output-file-path");
 
     // TODO: getopt
     char *infile = argv[1], *outfile = argv[2];
-    enum { ASSM_MODE, OBJ_MODE };
-    int mode = OBJ_MODE;
-    if (argc == 4) {
-        mode = ASSM_MODE;
-        infile = argv[2];
-        outfile = argv[3];
+
+    // detect filetype by file extensions, like gcc :smile:
+    enum { FT_UNK = -1, FT_C, FT_ASM, FT_OBJ };
+    int in_ft = FT_UNK, out_ft = FT_UNK;
+    char ft_table[128];                      // TODO: enough length?
+    memset(ft_table, -1, sizeof(ft_table));  // -1 is FT_UNK
+    ft_table['c'] = FT_C;
+    ft_table['s'] = FT_ASM;
+    ft_table['o'] = FT_OBJ;
+
+    in_ft = ft_table[infile[strlen(infile) - 1]];
+    out_ft = ft_table[outfile[strlen(outfile) - 1]];
+
+    if (in_ft == FT_UNK || out_ft == FT_UNK)
+        error("unknown (or may be invalid) input or output file type");
+    if (out_ft == FT_C) error("invalid output file type");
+    if (in_ft == FT_ASM || in_ft == FT_OBJ)
+        error("invalid input file type (but will be valid :muscle:)");
+
+    Vector *code;  // assembly
+
+    if (in_ft == FT_C) {
+        // compile
+        Vector *tokens = read_tokens_from_filepath(infile);
+        tokens = preprocess_tokens(tokens);
+        tokens = concatenate_string_literal_tokens(tokens);
+
+        Vector *asts = parse_prog(tokens);
+
+        Env *env = analyze_ast(asts);
+        optimize_asts_constant(asts, env);
+
+        code = generate_register_code(asts);
+        code = optimize_code(code);
     }
 
-    Vector *tokens = read_tokens_from_filepath(infile);
-    tokens = preprocess_tokens(tokens);
-    tokens = concatenate_string_literal_tokens(tokens);
-
-    Vector *asts = parse_prog(tokens);
-
-    Env *env = analyze_ast(asts);
-    optimize_asts_constant(asts, env);
-
-    Vector *code = generate_register_code(asts);
-    code = optimize_code(code);
-
     FILE *fh = fopen(outfile, "wb");
-    switch (mode) {
-        case ASSM_MODE:
+    switch (out_ft) {
+        case FT_ASM:
             for (int i = 0; i < vector_size(code); i++)
                 dump_code((Code *)vector_get(code, i), fh);
             break;
 
-        case OBJ_MODE: {
+        case FT_OBJ: {
+            // assemble
             ObjectImage *obj = assemble_code(code);
             dump_object_image(obj, fh);
         } break;
-
-        default:
-            assert(0);
     }
     fclose(fh);
 
--- test.sh
+++ test.sh
@@ -9,12 +9,12 @@ function fail(){
 ./aqcc test
 [ $? -eq 0 ] || fail "./aqcc test"
 
-./aqcc -S test_define.c _test.s
+./aqcc test_define.c _test.s
 gcc _test.s -o _test.o testutil.o -static
 ./_test.o
 
 gcc -E -P test.c -o _test.c
-./aqcc -S _test.c _test.s
+./aqcc _test.c _test.s
 gcc _test.s -o _test.o testutil.o -static
 ./_test.o
 
