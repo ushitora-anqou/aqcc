78d8366167e1ce95a20d9460892da4929b1df335
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2018-09-08 22:33:07 +0900

    _startを探してエントリポイントに設定するようにした。

--- link.c
+++ link.c
@@ -1,24 +1,61 @@
 #include "aqcc.h"
 
 struct ExeImage {
-    Vector *objs;  // vector<BinaryData *>
+    Vector *objs;  // vector<ObjectData *>
 };
 
-typedef struct BinaryData BinaryData;
-struct BinaryData {
+typedef struct ObjectData ObjectData;
+struct ObjectData {
     char *data;
     int size;
+
+    char *shdr, *symtab, *strtab;
+    int nshdr, nsymtab;
 };
 
-BinaryData *new_binary_data(char *data, int size)
+int read_byte(char *data) { return data[0] & 0xff; }
+
+int read_word(char *data)
 {
-    BinaryData *bin = (BinaryData *)safe_malloc(sizeof(BinaryData));
-    bin->data = data;
-    bin->size = size;
-    return bin;
+    return read_byte(data) | (read_byte(data + 1) << 8);
 }
 
-BinaryData *read_entire_binary(char *filepath)
+int read_dword(char *data)
+{
+    return read_word(data) | (read_word(data + 2) << 16);
+}
+
+ObjectData *new_object_data(char *data, int size)
+{
+    ObjectData *obj = (ObjectData *)safe_malloc(sizeof(ObjectData));
+    obj->data = data;
+    obj->size = size;
+    obj->shdr = obj->symtab = obj->strtab = NULL;
+
+    // parse data
+    obj->shdr = data + read_dword(data + 40);
+    char *section_strtab =
+        data + read_dword(obj->shdr + 0x40 * read_word(data + 62) + 24);
+    obj->nshdr = read_word(data + 60);
+    for (int i = 1; i < obj->nshdr; i++) {
+        char *entry = obj->shdr + 0x40 * i;
+        char *offset = data + read_dword(entry + 24);
+        int size = read_dword(entry + 32);
+        if (obj->symtab == NULL &&
+            strcmp(section_strtab + read_dword(entry), ".symtab") == 0) {
+            obj->symtab = offset;
+            obj->nsymtab = size / 24;
+        }
+        if (obj->strtab == NULL &&
+            strcmp(section_strtab + read_dword(entry), ".strtab") == 0)
+            obj->strtab = offset;
+    }
+    assert(obj->shdr != NULL && obj->symtab != NULL && obj->strtab != NULL);
+
+    return obj;
+}
+
+ObjectData *read_entire_binary(char *filepath)
 {
     FILE *fh = fopen(filepath, "rb");
     if (fh == NULL) error("no such binary file: '%s'", filepath);
@@ -31,7 +68,28 @@ BinaryData *read_entire_binary(char *filepath)
     fclose(fh);
 
     // string_builder_size() returns the size including a null character.
-    return new_binary_data(string_builder_get(sb), string_builder_size(sb) - 1);
+    return new_object_data(string_builder_get(sb), string_builder_size(sb) - 1);
+}
+
+int search_symbol(Vector *objs, const char *name)
+{
+    int prev_offset = 0;
+
+    for (int i = 0; i < vector_size(objs); i++) {
+        ObjectData *obj = (ObjectData *)vector_get(objs, i);
+        for (int j = 0; j < obj->nsymtab; j++) {
+            char *entry = obj->symtab + 0x30 * j;
+            if (strcmp(obj->strtab + read_dword(entry), name) != 0) continue;
+            int st_info = read_byte(entry + 4), st_shndx = read_word(entry + 6),
+                st_value = read_word(entry + 8);
+            if (st_shndx == 0) continue;
+            return prev_offset + read_dword(obj->shdr + 0x40 * st_shndx + 24) +
+                   st_value;
+        }
+        prev_offset += obj->size;
+    }
+
+    error("undefined symbol: %s", name);
 }
 
 ExeImage *link_objs(Vector *obj_paths)
@@ -132,13 +190,16 @@ void dump_exe_image(ExeImage *exeimg, FILE *fh)
     // *** BODY ***
     //
 
+    int body_offset = emitted_size();
+
     for (int i = 0; i < vector_size(exeimg->objs); i++) {
-        BinaryData *obj = (BinaryData *)vector_get(exeimg->objs, i);
+        ObjectData *obj = (ObjectData *)vector_get(exeimg->objs, i);
         for (int j = 0; j < obj->size; j++) emit_byte(obj->data[j]);
     }
 
     // rewrite placeholders
-    int ep_offset = 0x4000b8;  // TODO
+    int ep_offset =
+        0x400000 + body_offset + search_symbol(exeimg->objs, "_start");
     reemit_byte(ep_addr + 0, (ep_offset >> 0) & 0xff);
     reemit_byte(ep_addr + 1, (ep_offset >> 8) & 0xff);
     reemit_byte(ep_addr + 2, (ep_offset >> 16) & 0xff);
